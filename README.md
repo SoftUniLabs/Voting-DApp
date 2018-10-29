76f16590f9309124aafaf81ae4806bbdcbcb5416
Ivan Abadzhiev <abadzhievltd@gmail.com>
2018-10-29 10:55:19 +0200

    Initial commit: Upload the project

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Voting-DApp
-Example Dapp from the Blockchain Course
+# Client-Side Wallet - Demo
+
+Play with the demo now: https://client-side-wallet-demo.herokuapp.com
+
+This demon example demonstrates how to build a simple DApp (Voting) with client-side Ethereum wallet. Based on Solidity smart contract and client-side UI (JS + jQuery + Ethers.js) + server-side account management (Node.js + Express + simple REST API). The wallet is encrypted in UTC / JSON format (both private key + mnemonics are encrypted with AES-CRT-128 using SCrypt key derivation).
+ - The app implements register / login / logout / vote.
+ - Candidates and votes stay in Solidity contract on the Ropsten testnet.
+ - Users register in the system and get a wallet (private key + address)
+    - The wallet JSON is stored after strong encryption at the server side
+    - The wallet password / private key never leave the client app
+ - User login -> download the encrypted wallet JSON from the server
+ - After login / register, keep the encrypted wallet JSON in the browser session
+ - Logout -> clear the browser session
+ - Use different passwords for server login and for the wallet
+    - Wallet password (client-side): HMAC(password, username + 'wallet')
+    - Server password (wallet id): HMAC(username, password + 'server')
+ - Lost password cannot be recovered / reset -> use the mnemonics
+ - Based on the Ethers.js library
+ 
+## Project Structure
+
+This DApp example is based on JavaScript (server-side + client-side)
+ - `public` - holds the client-side JS app
+ - `src` - holds the server-side Node.js app
+ - `contract` - holds the `Voting` solidity contract, published at https://ropsten.etherscan.io/address/0x121241C506ebb1d04A4d8d355D37aC9fd06361de 
+
+## Running the Demo
+
+1. Download the demo from GitHub:
+```
+git clone https://github.com/SoftUniLabs/client-side-ethereum-wallet
+```
+
+2. Go to the project directory:
+```
+cd Voting-DApp
+```
+
+3. Install the NPM packages
+```
+npm install
+```
+
+3. Run the server
+```
+npm start
+```
+
+4. Open the project URL from your Web browser:
+```
+http://localhost:80
+```
+
+## Slides and Videos
+
+Slides: https://www.slideshare.net/nakov/clientside-wallets-nakov-at-ethereum-meetup-sofia-april-2018
+
+Video (in Bulgarian language): https://youtu.be/Wi9nZNUC_B8
+
+Video (in English): https://youtu.be/7b3Pr7DMSMc
+
+Screen only video (English): https://youtu.be/VvTlF4whjj8
+
+Video (in English) from BlockWorld 2018: https://youtu.be/_E_HPGbDRmk
---
+++ public/client.js
@@ -0,0 +1,279 @@
+$(document).ready(function () {
+    const ethereumProvider = ethers.providers.getDefaultProvider('ropsten');
+    
+    // The data for the contract (Address and ABI)
+    const votingContractAddress = "0x31b5de7684d085c622671c713de352d81ffbd3f1";
+    const votingContractABI = [
+        {
+            "constant": true,
+            "inputs": [{"name": "index", "type": "uint32"}],
+            "name": "getCandidate",
+            "outputs": [{"name": "", "type": "string"}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "candidatesCount",
+            "outputs": [{"name": "", "type": "uint32"}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{"name": "name", "type": "string"}],
+            "name": "addCandidate",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{"name": "index", "type": "uint32"}],
+            "name": "vote",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [{"name": "index", "type": "uint32"}],
+            "name": "getVotes",
+            "outputs": [{"name": "", "type": "uint32"}],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
+
+    //Make instance of the contract
+    const votingContract = new ethers.Contract(
+        votingContractAddress, votingContractABI, ethereumProvider);
+
+    showView("viewHome");
+
+    $('#linkHome').click(function () {
+        showView("viewHome");
+    });
+
+    $('#linkLogin').click(function () {
+        showView("viewLogin");
+    });
+
+    $('#linkRegister').click(function () {
+        showView("viewRegister");
+    });
+
+    $('#linkLogout').click(logout);
+
+    $('#buttonLogin').click(login);
+    $('#buttonRegister').click(register);
+
+    function showView(viewName) {
+        // Hide all views and show the selected view only
+        $('main > section').hide();
+        $('#' + viewName).show();
+        
+        const loggedIn = sessionStorage.jsonWallet;
+        
+        if (loggedIn) {
+            $('.show-after-login').show();
+            $('.hide-after-login').hide();
+        } else {
+            $('.show-after-login').hide();
+            $('.hide-after-login').show();
+        }
+        
+        if (viewName === 'viewHome') {
+            loadVotingResults();
+        }
+    }
+
+    // Attach AJAX "loading" event listener
+    $(document).on({
+        ajaxStart: function() { $("#ajaxLoadingBox").fadeIn(200) },
+        ajaxStop: function() { $("#ajaxLoadingBox").fadeOut(200) }
+    });
+
+    function showInfo(message) {
+        $('#infoBox>p').html(message);
+        $('#infoBox').show();
+
+        $('#infoBox>header').click(function () {
+            $('#infoBox').hide();
+        });
+    }
+
+    function showError(errorMsg, err) {
+        let msgDetails = "";
+        
+        if (err && err.responseJSON){
+            msgDetails = err.responseJSON.errorMsg;
+        }
+        
+        $('#errorBox>p').html('Error: ' + errorMsg + msgDetails);
+        $('#errorBox').show();
+        
+        $('#errorBox>header').click(function () {
+            $('#errorBox').hide();
+        });
+    }
+
+    function showProgressBox(percent) {
+        let msg = "Wallet encryption / decryption ... " +
+            Math.round(percent * 100) + "% complete";
+        $('#progressBox').html(msg).show(100);
+    }
+
+    function hideProgressProgress() {
+        $('#progressBox').hide(100);
+    }
+    
+    function deriveWalletPassword(username, password) {
+        return CryptoJS.HmacSHA256(password, 
+            username + 'wallet').toString();
+    }
+
+    async function register() {
+        
+        let username = $('#usernameRegister').val();
+        let password = $('#passwordRegister').val();
+        
+        try {
+            extraEntropy = '0x' + CryptoJS.HmacSHA256(
+                password, username + new Date()).toString();
+            let wallet = ethers.Wallet.createRandom({extraEntropy});
+            
+            walletPassword = deriveWalletPassword(username, password);
+            
+            let jsonWallet = await wallet.encrypt(
+                walletPassword, {}, showProgressBox);
+            
+            let walletId = CryptoJS.HmacSHA256(
+                username, password + 'server').toString();
+            
+            let result = await $.ajax({
+                type: 'POST',
+                url: `/register`,
+                data: JSON.stringify({walletId, jsonWallet}),
+                contentType: 'application/json'
+            });
+            
+            sessionStorage['username'] = username;
+            sessionStorage['jsonWallet'] = jsonWallet;
+            
+            showView("viewHome");
+            showInfo(`User "${username}" registered successfully. Please save your mnemonics: <b>${wallet.mnemonic}</b>`);
+        }
+        catch (err) {
+            showError("Cannot register user. ", err);
+        }
+        finally {
+            hideProgressProgress();
+        }
+    }
+
+    async function login() {
+        let username = $('#usernameLogin').val();
+        let password = $('#passwordLogin').val();
+        let walletId = CryptoJS.HmacSHA256(
+            username, password + 'server').toString();
+        
+        try {
+            
+            let result = await $.ajax({
+                type: 'POST',
+                url: `/login`,
+                data: JSON.stringify({walletId}),
+                contentType: 'application/json'
+            });
+
+            sessionStorage['username'] = username;
+            sessionStorage['jsonWallet'] = result.jsonWallet;
+            
+            showView("viewHome");
+            showInfo(`User "${username}" logged in successfully.`);
+        }
+        catch (err) {
+            showError("Cannot login user. ", err);
+        }
+    }
+
+    async function loadVotingResults() {
+        try {
+            // Load the candidates from the Ethereum smart contract
+            let candidates = [];
+            candidatesCount = await votingContract.candidatesCount();
+            
+            /* Loop all candidates and get candidate by id
+               In solidity it is not good practice to use loops for that reason we can 
+               loop client side. 
+            */
+            for (let index = 0; index < candidatesCount; index++) {
+                let candidate = await votingContract.getCandidate(index);
+                let votes = await votingContract.getVotes(index);
+                candidates.push({candidate, votes});
+            }
+
+            // Display the candidates
+            let votingResultsUl = $('#votingResults').empty();
+            for (let index = 0; index < candidatesCount; index++) {
+                let candidate = candidates[index];
+                let li = $('<li>').html(`${candidate.candidate} -> ${candidate.votes} votes `);
+                
+                if (sessionStorage['username']) {
+                    let button = $(`<input type="button" value="Vote">`);
+                    button.click(function () { vote(index, candidate.candidate) });
+                    li.append(button);
+                }
+
+                li.appendTo(votingResultsUl);
+            }
+        }
+        catch (err) {
+            showError(err);
+        }
+    }
+
+    async function vote(candidateIndex, candidateName) {
+        try {
+            let jsonWallet = sessionStorage['jsonWallet'];
+            let password = prompt("Enter your wallet password:");
+
+            walletPassword = deriveWalletPassword(
+                sessionStorage['username'], password);
+            
+            //Gte your wallet and decrypt it
+            let wallet = await ethers.Wallet.fromEncryptedWallet(
+                jsonWallet, walletPassword, showProgressBox);
+            let privateKey = wallet.privateKey;
+
+            //Make an instance of already existing contract
+            const votingContract = new ethers.Contract(
+                votingContractAddress, votingContractABI,
+                new ethers.Wallet(privateKey, ethereumProvider));
+            
+            // Make transaction to the contract
+            let votingResult = await votingContract.vote(candidateIndex);
+            let txHash = votingResult.hash;
+            showInfo(`Voted successfully for: ${candidateName}. See the transaction: <a href="https://ropsten.etherscan.io/tx/${txHash}" target="_blank">${txHash}</a>`);
+        }
+        catch (err) {
+            showError(err);
+        }
+        finally {
+            hideProgressProgress();
+        }
+    }
+
+    function logout() {
+        // Clear user data
+        sessionStorage.clear();
+        showView("viewHome");
+    }
+});
